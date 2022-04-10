b2778fde2cec585ad0b7c25a6f906a2dfed1d0ff
Brady Brown <brbrown@hubspot.com>
2022-04-09 21:33:03 -0400

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # asset-bender.nvim
 Automatically start an asset-bender process
+
+## Setup
+
+```lua
+require("packer").startup(function()
+
+    use({
+        "bobrown101/asset-bender.nvim",
+        config = function() require("asset-bender").setup({}) end
+    })
+
+end)
+```
+
+## How does it work?
+`asset-bender.nvim` creates an autocommand attached to "BufReadPost" (it is scoped under the group `asset-bender.nvim`).
+This autocommand will then do the following:
+1. Check to see that the current buffer is relevent (aka - is a javascript/typescript file)
+2. Find the closest git directory (signifying the project root)
+3. See if there is already an asset-bender process running for this project root, and if not, start one
+
+## Logging
+```bash
+tail -f ~/.cache/nvim/asset-bender.log
+```
+
--- lua/asset-bender-utils.lua
+++ lua/asset-bender-utils.lua
@@ -9,6 +9,16 @@ function M.is_dir(filename)
     return stat and stat.type == 'directory' or false
 end
 
+function M.dirname(filepath)
+    local is_changed = false
+    local result = filepath:gsub(M.path_sep .. "([^" .. M.path_sep .. "]+)$",
+                                 function()
+        is_changed = true
+        return ""
+    end)
+    return result, is_changed
+end
+
 function M.buffer_find_root_dir(bufnr, is_root_path)
     local bufname = vim.api.nvim_buf_get_name(bufnr)
     if vim.fn.filereadable(bufname) == 0 then return nil end
@@ -16,7 +26,7 @@ function M.buffer_find_root_dir(bufnr, is_root_path)
     -- Just in case our algo is buggy, don't infinite loop.
     for _ = 1, 100 do
         local did_change
-        dir, did_change = dirname(dir)
+        dir, did_change = M.dirname(dir)
         if is_root_path(dir, bufname) then return dir, bufname end
         -- If we can't ascend further, then stop looking.
         if not did_change then return nil end
