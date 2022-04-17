405d54c84e8b68e586ee75bc9036f777b069c054
Brady Brown <brbrown@hubspot.com>
2022-04-16 20:26:14 -0400

    bugfix

--- lua/asset-bender.lua
+++ lua/asset-bender.lua
@@ -37,9 +37,9 @@ end
 local function getLogPath() return vim.lsp.get_log_path() end
 
 local function startAssetBenderProcess(rootsArray)
-    local workspaces = reduce_array(rootsArray, function(accumulator, current, "")
+    local workspaces = reduce_array(rootsArray, function(accumulator, current)
         return accumulator .. " " .. current
-    end)
+    end, "")
 
     log.info('Asset Bender starting new client')
     log.info('starting NEW asset-bender with workspaces of:')
